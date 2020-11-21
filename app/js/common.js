a88523ee69916276ef15d19b63f2812fb8de164b
AlDekker <alexander-dekker@yandex.ru>
2020-11-21 18:41:43 +0300

    update

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){
-
-  // $("img, a").on("dragstart", function(event) { event.preventDefault(); });
-
-});
+document.addEventListener('DOMContentLoaded', function(event){
+  // Here
+})
